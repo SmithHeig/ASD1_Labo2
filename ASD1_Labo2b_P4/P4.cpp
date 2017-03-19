f1270a47c2583a7b5dd958f3b3ff7133ef3e51ee
Smithja <james.smith@heig-vd.ch>
2017-03-19 15:46:59 +0100

    update of the P4 class and P4.cpp

--- ASD1_Labo2b_P4/P4.cpp
+++ ASD1_Labo2b_P4/P4.cpp
@@ -1,4 +1,33 @@
 #include "P4.h"
 
-// A COMPLETER
+using namespace std;
 
+P4::P4(){
+   
+}
+
+void P4::reset(){
+   
+}
+    
+void P4::playInColumn(size_t c, Player p){
+   
+}
+    
+bool P4::isWinner(Player p) const{
+   
+}
+    
+bool P4::isValidMove(size_t c) const{
+   
+}
+    
+size_t P4::chooseNextMove(Player p, unsigned depth){
+   
+}
+
+string P4::getName() const{
+   
+}
+
+#endif /* P4_h */
